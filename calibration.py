cf15a267289dc04e7edb21302550058b4d780bde
enriquepb01 <perezenr@usc.edu>
2023-11-05 22:34:40 -0800

    calibration and path prediction code

---
+++ calibration.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Calibration():
+    def __init__(self, tr, tl, br, bl, table_width, table_height):
+        self.points = [tr, tl, br, bl]
+        self.table_width = table_width
+        self.table_height = table_height
+        self.H = []
+
+    def find_transformation_matrix(self):
+        A = []
+        p1 = self.points
+        p2 = [[0,self.table_height/2], [-self.table_width,self.table_height/2], [0,-self.table_height/2], [-self.table_width,-self.table_height/2]]
+        for i in range(4):
+            x, y = p1[i][0], p1[i][1]
+            u, v = p2[i][0], p2[i][1]
+            A.append([x, y, 1, 0, 0, 0, -u * x, -u * y, -u])
+            A.append([0, 0, 0, x, y, 1, -v * x, -v * y, -v])
+        A = np.asarray(A)
+        print(A)
+        U, S, Vh = np.linalg.svd(A)
+        L = Vh[-1, :] / Vh[-1, -1]
+        H = L.reshape(3, 3)
+        self.H = H
+        return H
+    
+    def perform_transformation(self, p1):
+        return np.matmul(self.H, np.array([[p1[0]], [p1[1]], [1]]))
--- run.py
+++ run.py
@@ -1,8 +1,28 @@
-from ball_tracking import PathPrediction
+from path_prediction import PathPrediction
+from calibration import Calibration
 
-x1 = -10
-y1=  0
-x2 = -9
-y2 = 1.5
-path = PathPrediction(x1, y1, x2, y2)
-print(path.find_path_end())
+TABLE_WIDTH = 10
+TABLE_HEIGHT =  5
+
+# Calibration
+tr_x = 0
+tr_y = 5
+tl_x = -10
+tl_y = 5
+br_x = 0
+br_y = 0
+bl_x = -10
+bl_y = 0
+cal = Calibration([tr_x,tr_y], [tl_x,tl_y], [br_x,br_y], [bl_x,bl_y], TABLE_WIDTH, TABLE_HEIGHT)
+cal.find_transformation_matrix()
+prj = cal.perform_transformation([0,2.5])
+print(prj)
+
+# # Path prediction
+# x1 = -10
+# y1=  0
+# x2 = -9
+# y2 = 1.5
+# path = PathPrediction(x1, y1, x2, y2)
+# print(path.find_path_end())
+
